081d051e59f1f79df8017530b76bf5fab2d493f9
Brigham Toskin <brigham@ionoclast.com>
2014-08-16 09:53:49 -0700

    Initial commit, gwget's main.go stub.

---
+++ gwget/main.go
@@ -0,0 +1,26 @@
+// main.cpp
+// gwget main package
+//
+// Copyright © 2014 Brigham Toskin
+// This software is part of the GOSIX source distribution. It is distributable
+// under the terms of a modified MIT License. You should have received a copy of
+// the license in the file LICENSE. If not, see:
+// <http://code.google.com/p/rogue-op/wiki/LICENSE>
+//
+// Formatting:
+//	utf-8 ; unix ; 80 cols ; tabwidth 4
+////////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	// stub
+}
