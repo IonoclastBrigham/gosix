4e6fc77a44c688210edc38e647680cfeadf20adc
Brigham Toskin <brigham@ionoclast.com>
2014-08-18 08:21:03 -0700

    Header comment fixups.
    Fix erroneous .cpp extension in top file declaration.
    Update license ref to the one in the github repo.

--- gecho/main.go
+++ gecho/main.go
@@ -1,11 +1,11 @@
-// main.cpp
+// main.go
 // gecho main package
 //
 // Copyright © 2014 Brigham Toskin
 // This software is part of the GOSIX source distribution. It is distributable
 // under the terms of a modified MIT License. You should have received a copy of
 // the license in the file LICENSE. If not, see:
-// <http://code.google.com/p/rogue-op/wiki/LICENSE>
+// <https://github.com/IonoclastBrigham/gosix/blob/master/LICENSE>
 //
 // Formatting:
 //	utf-8 ; unix ; 80 cols ; tabwidth 4
